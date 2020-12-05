144e7c81520a865e10678b968621afb35627c851
Константин Бедрицкий <kmb2@tpu.ru>
2020-12-05 19:34:09 +0600

    Texture class

---
+++ src/texture.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <glad/glad.h>
+
+class Texture
+{
+public:
+  Texture();
+  ~Texture();
+  void Bind();
+  void Generate(unsigned int width, unsigned int height, unsigned char *data);
+
+  unsigned int internal_format;
+  unsigned int image_format;
+  unsigned int wrap_s;
+  unsigned int wrap_t;
+  unsigned int filter_min;
+  unsigned int filter_mag;
+
+private:
+  unsigned int ID_;
+  unsigned int width_;
+  unsigned int height_;
+};
