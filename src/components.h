53585008b71d90cb4e62941ba4cf4cccb1fad554
Константин Бедрицкий <kmb2@tpu.ru>
2020-12-10 20:03:48 +0600

    Transform component: "scale" fireld renamed to "size"

--- src/components.h
+++ src/components.h
@@ -8,17 +8,17 @@
 struct Transform
 {
   glm::vec3 position;
-  glm::vec3 scale;
+  glm::vec2 size;
   float rotation;
-  glm::vec3 anchor;
-  Transform() : position(0.0f), scale(512.0f / 4.0f, 128.0f / 4.0f, 0.0f), rotation(0.0f), anchor(0.5f, 0.5f, 0.0f){};
+  glm::vec2 anchor;
+  Transform() : position(0.0f), size(128.0f, 32.0f), rotation(0.0f), anchor(0.5f, 0.5f){};
   glm::mat4 Model() const
   {
     glm::mat4 model = glm::mat4(1.0f);
     model = glm::translate(model, position);
     model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 0.0f, 1.0f));
-    model = glm::translate(model, -anchor * scale);
-    model = glm::scale(model, scale);
+    model = glm::translate(model, glm::vec3(-anchor * size, 0.0f));
+    model = glm::scale(model, glm::vec3(size, 0.0f));
 
     return model;
   }
